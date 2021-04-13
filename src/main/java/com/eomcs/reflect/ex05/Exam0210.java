c852ef04d95edbfe2e31b3c223a232aa3c4e80f6
dong3512 <dlehddls1015@naver.com>
2021-04-13 12:28:58 +0900

    .

---
+++ src/main/java/com/eomcs/reflect/ex05/Exam0210.java
@@ -0,0 +1,57 @@
+// reflection API - 배열 항목의 타입 알아내기
+package com.eomcs.reflect.ex05;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+public class Exam0210 {
+
+  public static void main(String[] args) throws Exception {
+    System.out.println(byte.class.getName());  // byte 타입
+    System.out.println(byte[].class.getName()); // byte 배열 타입
+    System.out.println("---------------------");
+
+    System.out.println(short.class.getName());  // byte 타입
+    System.out.println(short[].class.getName()); // byte 배열 타입
+    System.out.println("---------------------");
+
+    System.out.println(long.class.getName());  // byte 타입
+    System.out.println(long[].class.getName()); // byte 배열 타입
+    System.out.println("---------------------");
+
+    System.out.println(int.class.getName());  // byte 타입
+    System.out.println(int[].class.getName()); // byte 배열 타입
+    System.out.println("---------------------");
+
+    System.out.println(float.class.getName());  // byte 타입
+    System.out.println(float[].class.getName()); // byte 배열 타입
+    System.out.println("---------------------");
+
+    System.out.println(double.class.getName());  // byte 타입
+    System.out.println(double[].class.getName()); // byte 배열 타입
+    System.out.println("---------------------");
+
+    System.out.println(String.class.getName());  // byte 타입
+    System.out.println(String[].class.getName()); // byte 배열 타입
+    System.out.println("---------------------");
+
+    System.out.println(char.class.getName());  // byte 타입
+    System.out.println(char[].class.getName()); // byte 배열 타입
+    System.out.println("---------------------");
+
+    System.out.println(Date.class.getName());  // byte 타입
+    System.out.println(Date[].class.getName()); // byte 배열 타입
+    System.out.println("---------------------");
+
+    System.out.println(ArrayList.class.getName());  // byte 타입
+    System.out.println(ArrayList[].class.getName()); // byte 배열 타입
+    System.out.println("---------------------");
+
+  }
+
+}
+
+
+
+
+
